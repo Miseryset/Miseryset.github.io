2142703f0dea5045fd634bfbce7b46cc02f8f47b
Miseryset <1598654374@qq.com>
2023-01-04 12:50:48 +0800

    update

--- scripts/hexo.js
+++ scripts/hexo.js
@@ -1,3 +1,3 @@
 hexo.extend.injector.register('body_end', '<script src="/js/pauseaudio.js"></script>', 'post');
-hexo.extend.injector.register('body_end', '<script src="/js/autoplay.js"></script>', 'about');
-hexo.extend.injector.register('body_begin', '<audio id="music" autoplay loop preload="auto"><source src="https://static-res.qq.com/static-res/pandian2022/home-bgm-1.mp3" type="audio/mpeg"></audio>', 'about');
+//hexo.extend.injector.register('body_end', '<script src="/js/autoplay.js"></script>', 'about');
+//hexo.extend.injector.register('body_begin', '<audio id="music" autoplay loop preload="auto"><source src="https://static-res.qq.com/static-res/pandian2022/home-bgm-1.mp3" type="audio/mpeg"></audio>', 'about');
