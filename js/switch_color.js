04b8d8bb380a53f199765c7f573be4e8c593388a
Miseryset <Miseryset@users.noreply.github.com>
2023-05-20 13:24:41 +0000

    deploy: b73b8f6a29e849609d4bb6be49c2c1602f07fdbb

--- js/switch_color.js
+++ js/switch_color.js
@@ -1,10 +1,15 @@
 document.write('<link type="text/css" rel="stylesheet" href="/css/global.css" />');
-var d = new Date();
-var h = d.getHours();
-if (h > 8 && h < 18) {
-  document.documentElement.style.setProperty('--global-main-color', "#ffc0cb");
-  // document.write('<link type="text/css" rel="stylesheet" href="/css/global_c1.css" />');
-} else {
-  document.documentElement.style.setProperty('--global-main-color', "#a7a4db");
-  // document.write('<link type="text/css" rel="stylesheet" href="/css/global_c2.css" />');
-}
+// var d = new Date();
+// var h = d.getHours();
+// if (h > 8 && h < 18) {
+  // document.documentElement.style.setProperty('--global-main-color', "#ffc0cb");
+  // // document.write('<link type="text/css" rel="stylesheet" href="/css/global_c1.css" />');
+// } else {
+  // document.documentElement.style.setProperty('--global-main-color', "#a7a4db");
+  // // document.write('<link type="text/css" rel="stylesheet" href="/css/global_c2.css" />');
+// }
+var r=Math.round(Math.random()*255);
+var g=Math.round(Math.random()*255);
+var b=Math.round(Math.random()*255);
+
+document.documentElement.style.setProperty('--global-main-color', "rgb("+r+","+g+","+b+")");
