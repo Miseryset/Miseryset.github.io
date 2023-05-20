6c73b5f5da9786352e20a140314aea57e3697ada
Miseryset <Miseryset@users.noreply.github.com>
2023-05-20 07:54:41 +0000

    deploy: 12b55b7fa3746f15947ee690934abb054a88600a

--- js/switch_color.js
+++ js/switch_color.js
@@ -1,7 +1,10 @@
+document.write('<link type="text/css" rel="stylesheet" href="/css/global.css" />');
 var d = new Date();
 var h = d.getHours();
 if (h > 8 && h < 18) {
-  document.write('<link type="text/css" rel="stylesheet" href="/css/global_c1.css" />');
+  document.documentElement.style.setProperty('--global-main-color', "#ffc0cb");
+  // document.write('<link type="text/css" rel="stylesheet" href="/css/global_c1.css" />');
 } else {
-  document.write('<link type="text/css" rel="stylesheet" href="/css/global_c2.css" />');
+  document.documentElement.style.setProperty('--global-main-color', "#a7a4db");
+  // document.write('<link type="text/css" rel="stylesheet" href="/css/global_c2.css" />');
 }
