263ddcf9609bdfc0fe4376bb9c3647875f3915f9
Miseryset <1598654374@qq.com>
2024-01-05 21:12:06 +0800

    Fix_an_error

--- source/js/pauseaudio.js
+++ source/js/pauseaudio.js
@@ -1,13 +1,15 @@
 var audios = document.getElementsByTagName("audio");
 // 暂停函数
 function pauseAll() {
-    var self = this;
-    [].forEach.call(audios, function(i) {
+    var currentAudio = this;
+    [].forEach.call(audios, function(audio) {
         // 将audios中其他的audio全部暂停
-        i !== self && i.pause();
-    })
+        if (audio !== currentAudio && !audio.paused) {
+            audio.pause();
+        }
+    });
 }
 // 给play事件绑定暂停函数
-[].forEach.call(audios, function(i) {
-    i.addEventListener("play", pauseAll.bind(i));
-})
+[].forEach.call(audios, function(audio) {
+    audio.addEventListener("play", pauseAll);
+});
